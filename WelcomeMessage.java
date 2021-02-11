83320855515ea3d1dadce927b74ce1bdb6cd98a5
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-11 18:28:20 +0530

    Second Commit

--- WelcomeMessage.java
+++ WelcomeMessage.java
@@ -4,6 +4,7 @@ public class WelcomeMessage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
+		System.out.println("Hello message");
 
 	}
 
